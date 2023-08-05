520a22329de04e1fa9d5f57fe63f09e30a46d4d6
zaharenkoliza <Liza93087@gmail.com>
2023-08-05 17:23:47 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const a = document.querySelector('.font1');
+
+const btn = document.querySelector('.name1');
+console.log(btn);
+btn.addEventListener('mousemove', function(){
+   a.classList.remove('hidden');
+   a
+   
+})
